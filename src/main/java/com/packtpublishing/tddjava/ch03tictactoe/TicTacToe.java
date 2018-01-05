ec30ff45b24b7417dce19be50061114153ac211c
AlexBMA <burghelea.alex@gmail.com>
2018-01-05 14:13:20 +0200

    2 commit

--- src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToe.java
@@ -1,11 +1,39 @@
 package com.packtpublishing.tddjava.ch03tictactoe;
 
+
 public class TicTacToe {
 
-    public void play(int x, int y) {
+    private Character[][] board={{'\0','\0','\0'},{'\0','\0','\0'},{'\0','\0','\0'}};
+    private char lastPlayer = '\0';
 
-        if(x<1 || x>3){
-            throw  new RuntimeException("X outside the board");
+    public String play(int x, int y) {
+
+        checkAxis(x);
+        checkAxis(y);
+        setBox(x,y);
+        lastPlayer = nextPlayer();
+        return "No Winner";
+    }
+
+    private void checkAxis(int axis){
+        if(axis<1 || axis>3){
+            throw
+            new RuntimeException("X is outside board");
         }
+    }
+    private void setBox(int x,int y){
+        if(board[x-1][y-1] !='\0'){
+            throw
+            new RuntimeException("Box is occupied");
+        } else {
+            board[x-1][y-1] = 'X';
+        }
+    }
+
+    public char nextPlayer() {
+        if(lastPlayer =='X'){
+            return 'O';
+        }
+        return 'X';
     }
 }
--- src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch03tictactoe/TicTacToeSpec.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
 
 public class TicTacToeSpec {
 
@@ -15,11 +16,35 @@ public class TicTacToeSpec {
     public final void before(){
         ticTacToe = new TicTacToe();
     }
-
     @Test
-    public void whenXOutsideBoardThenRuntimeException()
-    {
+    public void whenXOutsideBoardThenRuntimeException(){
         exception.expect(RuntimeException.class);
         ticTacToe.play(5,2);
+    }
+    @Test
+    public void whenYOutsideBoardThenRuntimeException(){
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2,5);
+    }
+    @Test
+    public void whenOccupiedThenRuntimeException(){
+        ticTacToe.play(2,1);
+        exception.expect(RuntimeException.class);
+        ticTacToe.play(2,1);
+    }
+    @Test
+    public void giveFirstTurnNextPlayerThenX(){
+        ticTacToe.play(1,1);
+        assertEquals('X',ticTacToe.nextPlayer());
+    }
+    @Test
+    public void givenLastTurnWasXWhenNextPlayerThen(){
+        ticTacToe.play(1,1);
+        assertEquals('O',ticTacToe.nextPlayer());
+    }
+    @Test
+    public void whenPlayThenNoWinner(){
+        String actual = ticTacToe.play(1,1);
+        assertEquals("No winner",actual);
     }
 }
